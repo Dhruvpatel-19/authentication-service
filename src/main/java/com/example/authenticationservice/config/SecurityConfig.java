b1cc14e43c9140442fab815a48dbaecdf3ca608a
Dhruv <dhruv.p@simformsolutions.com>
2022-08-04 17:03:00 +0530

    Removed WebSecurityConfigurerAdapter as it is deprecated

--- src/main/java/com/example/authenticationservice/config/SecurityConfig.java
+++ src/main/java/com/example/authenticationservice/config/SecurityConfig.java
@@ -4,36 +4,43 @@ import com.example.authenticationservice.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig{
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+   @Bean
+   protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+      http.csrf().disable();
 
-        http
-                .authorizeRequests()
-                .antMatchers("/owner/test").hasRole("OWNER")
-                .antMatchers("/user/test").hasRole("USER")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-    }
+      http
+              .authorizeRequests()
+              .antMatchers("/owner/test").hasRole("OWNER")
+              .antMatchers("/user/test").hasRole("USER")
+              .anyRequest()
+              .authenticated()
+              .and()
+              .httpBasic();
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder());
+      return http.build();
+   }
+
+    @Bean
+    protected DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return daoAuthenticationProvider;
     }
 
     @Bean
