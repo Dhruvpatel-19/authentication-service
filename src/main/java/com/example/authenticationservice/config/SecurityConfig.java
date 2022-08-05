cd6c7b04a2896b376d24e1d753c6404f86f9dab3
Dhruv <dhruv.p@simformsolutions.com>
2022-08-05 17:24:11 +0530

    Added JwtAuthenticationEntryPoint to handle unauthorized access and added sessionManagement in SecurityConfig

--- src/main/java/com/example/authenticationservice/config/SecurityConfig.java
+++ src/main/java/com/example/authenticationservice/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.authenticationservice.config;
 
+import com.example.authenticationservice.jwt.config.JwtAuthenticationEntryPoint;
 import com.example.authenticationservice.jwt.config.JwtAuthenticationFilter;
 import com.example.authenticationservice.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,6 +26,9 @@ public class SecurityConfig{
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
    @Bean
    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
       http.csrf().disable();
@@ -35,6 +40,9 @@ public class SecurityConfig{
               .antMatchers("/user/test").hasRole("USER")
               .anyRequest()
               .authenticated()
+              .and()
+              .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+              .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
               .and()
               .httpBasic();
 
